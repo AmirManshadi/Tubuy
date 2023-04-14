de066d9ac3baf5f5225c7c424bff323924ad83b5
amirmanshadi <amirmanshadi.code@gmail.com>
2023-04-14 18:34:25 +0430

    initialized firebase app with firebaseConfig

---
+++ main.js
@@ -0,0 +1,17 @@
+// * Firebase SDKs 
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// * Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyBMH9lnqgvgLx8c3uPW9muMKHeEUFDc7Sw",
+  authDomain: "tubuy-20c03.firebaseapp.com",
+  projectId: "tubuy-20c03",
+  storageBucket: "tubuy-20c03.appspot.com",
+  messagingSenderId: "986814514287",
+  appId: "1:986814514287:web:73b71cc66335a09117b99a"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
