2f5750d7d614485ff32029dd8f8b079fbcd27cab
amirmanshadi <amirmanshadi.code@gmail.com>
2023-04-16 18:13:01 +0430

    linked the database and items endpoint to project

--- main.js
+++ main.js
@@ -1,7 +1,17 @@
 // * Firebase Configuration object
-import FIREBASE_CONFIG from "./firebase.config";
-// * Firebase SDKs 
-import { initializeApp } from "firebase/app";
+import FIREBASE_CONFIG from "./firebase.config"
+// * Firebase SDKs
+import { initializeApp } from "firebase/app"
+import { getFirestore, collection, getDocs, addDoc } from "firebase/firestore"
 
 // Initialize Firebase
-const app = initializeApp(FIREBASE_CONFIG);
+const app = initializeApp(FIREBASE_CONFIG)
+
+// Get DataBase
+const db = getFirestore(app)
+
+// Create Collection Reference
+const collectionRef = collection(db, "items")
+
+// Real-Time Collection Data Fetch
+// todo: create a subscription (real-time database listner)
