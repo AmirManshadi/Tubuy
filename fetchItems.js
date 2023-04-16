ee1421c57558494b7b1b7a1cfbf084d9af4c2fc7
amirmanshadi <amirmanshadi.code@gmail.com>
2023-04-16 18:34:02 +0430

    created fetchItems function to extract each item from the items collection recieved from firestore database

---
+++ fetchItems.js
@@ -0,0 +1,10 @@
+export default function fetchItems (docs) {
+  const items = docs.map(doc => {
+    return {
+      itemId: doc.id,
+      itemValue: doc.data().item
+    }
+  });
+
+  return items
+}
