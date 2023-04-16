98d06e5b5ec5a8fb1a3b342e404a48f4d3a94c91
amirmanshadi <amirmanshadi.code@gmail.com>
2023-04-16 18:35:43 +0430

    imported fetchItems.js and renderItems.js into main.js

--- main.js
+++ main.js
@@ -2,7 +2,10 @@
 import FIREBASE_CONFIG from "./firebase.config"
 // * Firebase SDKs
 import { initializeApp } from "firebase/app"
-import { getFirestore, collection, getDocs, addDoc } from "firebase/firestore"
+import { getFirestore, collection, getDocs, addDoc, onSnapshot } from "firebase/firestore"
+// * Utilities
+import fetchItems from "./fetchItems"
+import renderItems from "./renderItems"
 
 // Initialize Firebase
 const app = initializeApp(FIREBASE_CONFIG)
@@ -15,3 +18,7 @@ const collectionRef = collection(db, "items")
 
 // Real-Time Collection Data Fetch
 // todo: create a subscription (real-time database listner)
+onSnapshot(collectionRef, (snapshot) => {
+  const items = fetchItems(snapshot.docs)
+  renderItems(items)
+})
