d3a38f14908ec68f4fc9ba8e3e9b75978f5286cb
amirmanshadi <amirmanshadi.code@gmail.com>
2023-04-16 15:35:43 +0430

    modularized firebase config object

--- main.js
+++ main.js
@@ -1,17 +1,7 @@
+// * Firebase Configuration object
+import FIREBASE_CONFIG from "./firebase.config";
 // * Firebase SDKs 
 import { initializeApp } from "firebase/app";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
-
-// * Firebase configuration
-const firebaseConfig = {
-  apiKey: "AIzaSyBMH9lnqgvgLx8c3uPW9muMKHeEUFDc7Sw",
-  authDomain: "tubuy-20c03.firebaseapp.com",
-  projectId: "tubuy-20c03",
-  storageBucket: "tubuy-20c03.appspot.com",
-  messagingSenderId: "986814514287",
-  appId: "1:986814514287:web:73b71cc66335a09117b99a"
-};
 
 // Initialize Firebase
-const app = initializeApp(firebaseConfig);
+const app = initializeApp(FIREBASE_CONFIG);
