85c5fe6d4c71ea89b7912e71dada54d5b4adcd01
amirmanshadi <amirmanshadi.code@gmail.com>
2023-04-16 18:57:49 +0430

    added submit event listner to add item to database

--- main.js
+++ main.js
@@ -2,7 +2,13 @@
 import FIREBASE_CONFIG from "./firebase.config"
 // * Firebase SDKs
 import { initializeApp } from "firebase/app"
-import { getFirestore, collection, getDocs, addDoc, onSnapshot } from "firebase/firestore"
+import {
+	getFirestore,
+	collection,
+	getDocs,
+	addDoc,
+	onSnapshot,
+} from "firebase/firestore"
 // * Utilities
 import fetchItems from "./fetchItems"
 import renderItems from "./renderItems"
@@ -18,8 +24,17 @@ const db = getFirestore(app)
 const collectionRef = collection(db, "items")
 
 // Real-Time Collection Data Fetch
-onSnapshot(collectionRef, (snapshot) => {
-  clearList()
-  const items = fetchItems(snapshot.docs)
-  renderItems(items)
+onSnapshot(collectionRef, snapshot => {
+	clearList()
+	const items = fetchItems(snapshot.docs)
+	renderItems(items)
+})
+
+// Event Listners
+document.getElementById("input-form").addEventListener("submit", e => {
+  e.preventDefault()
+	const { target: form } = e
+	addDoc(collectionRef, { item: form.item.value })
+		.then(() => console.log("collection updated"))
+		.catch(err => console.error(err.message))
 })
